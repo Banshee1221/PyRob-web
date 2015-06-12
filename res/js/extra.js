87bb47735543a9cbe4eff27d4d2d89d42019c994
Eugene <banshee1221@gmail.com>
2015-06-12 14:26:02 +0200

    Compatibility fixes

--- res/js/extra.js
+++ res/js/extra.js
@@ -17,25 +17,6 @@
 //    }
 //    return rv;
 //}
-
-function get_browser_info(){
-    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    if(/trident/i.test(M[1])){
-        tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
-        return {name:'IE',version:(tem[1]||'')};
-    }
-    if(M[1]==='Chrome'){
-        tem=ua.match(/\bOPR\/(\d+)/)
-        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-    }
-    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
-    return {
-        name: M[0],
-        version: M[1]
-    };
-}
-
 $( document ).ready(function(){
     $('.button-collapse').sideNav({
             menuWidth: 240,
@@ -51,33 +32,7 @@ $( document ).ready(function(){
     $('.materialboxed').materialbox();
 
     //credit to kennebec https://stackoverflow.com/users/80860/kennebec - stackoverflow
-    var browser = get_browser_info();
-    //alert(browser.name + browser.version);
 
-    if (browser.name.toString() == "Chrome"){
-        alert("Chrome!")
-        if (browser.version < 35){
-            window.location.replace("http://people.cs.uct.ac.za/~dbseug001/res/pages/browserWarning.html");
-        }
-    }
-    if (browser.name.toString() == "IE" || browser.name.toString() == "MSIE"){
-        //alert("IE!");
-        $("#teamModal").removeClass("modal-fixed-footer");
-        $("#supModal").removeClass("modal-fixed-footer");
-        if (browser.version < 10){
-            window.location.replace("http://people.cs.uct.ac.za/~dbseug001/res/pages/browserWarning.html");
-        }
-    }
-    if (browser.name.toString() == "Firefox"){
-        if (browser.version < 31){
-            window.location.replace("http://people.cs.uct.ac.za/~dbseug001/res/pages/browserWarning.html");
-        }
-    }
-    if (browser.name.toString() == "Safari"){
-        if (browser.version < 7) {
-            window.location.replace("http://people.cs.uct.ac.za/~dbseug001/res/pages/browserWarning.html");
-        }
-    }
 
 
     //var ieVer = getInternetExplorerVersion();
