e54ee9ca94794f8cae3b2dc7b362157763c93f5f
Eugene <banshee1221@gmail.com>
2015-06-12 13:51:25 +0200

    added browser checks

--- res/js/extra.js
+++ res/js/extra.js
@@ -1,21 +1,39 @@
-function getInternetExplorerVersion()
-{
-    var rv = -1;
-    if (navigator.appName == 'Microsoft Internet Explorer')
-    {
-        var ua = navigator.userAgent;
-        var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-        if (re.exec(ua) != null)
-            rv = parseFloat( RegExp.$1 );
+//function getInternetExplorerVersion()
+//{
+//    var rv = -1;
+//    if (navigator.appName == 'Microsoft Internet Explorer')
+//    {
+//        var ua = navigator.userAgent;
+//        var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+//        if (re.exec(ua) != null)
+//            rv = parseFloat( RegExp.$1 );
+//    }
+//    else if (navigator.appName == 'Netscape')
+//    {
+//        var ua = navigator.userAgent;
+//        var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+//        if (re.exec(ua) != null)
+//            rv = parseFloat( RegExp.$1 );
+//    }
+//    return rv;
+//}
+
+function get_browser_info(){
+    var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=/\brv[ :]+(\d+)/g.exec(ua) || [];
+        return {name:'IE',version:(tem[1]||'')};
     }
-    else if (navigator.appName == 'Netscape')
-    {
-        var ua = navigator.userAgent;
-        var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
-        if (re.exec(ua) != null)
-            rv = parseFloat( RegExp.$1 );
+    if(M[1]==='Chrome'){
+        tem=ua.match(/\bOPR\/(\d+)/)
+        if(tem!=null)   {return {name:'Opera', version:tem[1]};}
     }
-    return rv;
+    M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
+    return {
+        name: M[0],
+        version: M[1]
+    };
 }
 
 $( document ).ready(function(){
@@ -32,11 +50,43 @@ $( document ).ready(function(){
     });
     $('.materialboxed').materialbox();
 
-    var ieVer = getInternetExplorerVersion();
-    if (ieVer > 0){
-        alert("true");
-        $("#teamModal").removeClass("modal-fixed-footer");
-        $("#supModal").removeClass("modal-fixed-footer");
+    //credit to kennebec https://stackoverflow.com/users/80860/kennebec - stackoverflow
+    var browser = get_browser_info();
+    //alert(browser.name + browser.version);
+
+    if (browser.name.toString() == "Chrome"){
+        alert("Chrome!")
+        if (browser.version < 35){
+            window.location.replace("http://people.cs.uct.ac.za/~dbseug001/pages/browserWarning.html");
+        }
     }
+    if (browser.name.toString() == "IE"){
+        if (browser.version < 10){
+            window.location.replace("http://people.cs.uct.ac.za/~dbseug001/pages/browserWarning.html");
+        }
+    }
+    if (browser.name.toString() == "Firefox"){
+        if (browser.version < 31){
+            window.location.replace("http://people.cs.uct.ac.za/~dbseug001/pages/browserWarning.html");
+        }
+    }
+    if (browser.name.toString() == "Safari"){
+        if (browser.version < 7) {
+            window.location.replace("http://people.cs.uct.ac.za/~dbseug001/pages/browserWarning.html");
+        }
+    }
+
+
+
+
+
+    //var ieVer = getInternetExplorerVersion();
+    //if (ieVer >= 10){
+    //    $("#teamModal").removeClass("modal-fixed-footer");
+    //    $("#supModal").removeClass("modal-fixed-footer");
+    //}
+    //if (ieVer > 0 && ieVer < 12){
+    //
+    //}
 })
 
